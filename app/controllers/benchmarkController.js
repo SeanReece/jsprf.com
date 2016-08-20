55fd6e525a6863344dfe92a1e29fb046b7dc206a
Sean Reece <Sean.Reece@qlik.com>
2016-08-19 23:23:01 -0400

    Provide better benchmark results 🎉

--- app/controllers/benchmarkController.js
+++ app/controllers/benchmarkController.js
@@ -1,27 +1,43 @@
 angular.module('jsprf.controllers')
 
   .controller('benchmarkController', ['$scope', function ($scope) {
+    $scope.editMode = true
     var benchmarkSuite
     /**
      * Pass in the "winning" benchmarks from the benchmark.js suite and this will mark the winners in $scope.scripts
      * @param  {Array} winningScripts [description]
      * @return {[type]}                [description]
      */
-    function highlightWinners (winningScripts) {
+    function highlightWinners (suite) {
+      var completed = suite.filter('successful')
+      var winners = suite.filter('fastest')
+      var losers = suite.filter('slowest')
+
       $scope.$apply(function () {
-        $scope.scripts.forEach(function (script) {
-          var found = _.find(winningScripts, {'name': script.name})
-          script.winner = !!found
-        })
+        if (completed.length > 1) {
+          $scope.scripts.forEach(function (script) {
+            if (script.results) {
+              var found = _.find(winners, {'name': script.name})
+              if (found) {
+                script.winner = true
+                script.factor = (script.results.hz / losers[0].hz).toFixed(2) + "x faster"
+              }
+              else {
+                script.loser = !!_.find(losers, {'name': script.name})
+                script.factor = (script.results.hz / winners[0].hz).toFixed(2) + "x slower"
+              }
+            }
+          })
+        }
       })
     }
-    
+
     /**
      * Just remove any properties from the scripts that were added during the last benchmark run
      */
     function resetBenchmarks() {
         $scope.scripts.forEach(function (script){
-            delete script.errorMessage
+            delete script.error
             delete script.results
         })
     }
@@ -31,9 +47,9 @@ angular.module('jsprf.controllers')
      * @type {Array}
      */
     $scope.scripts = [
-      {content: "/o/.test('Hello World!');"},
-      {content: "'Hello World!'.indexOf('o') > -1;"},
-      {content: "!!'Hello World!'.match(/o/);"}
+      {content: "/o/.test('Hello World!');", name: "script1"},
+      {content: "'Hello World!'.indexOf('o') > -1;", name: "script2"},
+      {content: "!!'Hello World!'.match(/o/);", name: "script3"}
     ]
 
     $scope.addScript = function () {
@@ -49,12 +65,14 @@ angular.module('jsprf.controllers')
      * @return {[type]} [description]
      */
     $scope.runPerfs = function () {
+      $scope.editMode = false
       resetBenchmarks()
       benchmarkSuite = new Benchmark.Suite
       var count = 1
       // add tests
       $scope.scripts.forEach(function (script) {
-        script.name = 'script' + count++
+        script.name = script.name || 'script' + count
+        count++
         benchmarkSuite.add(script.name, script.content, {
           onStart: function (event) {
             $scope.$apply(function () {
@@ -63,13 +81,15 @@ angular.module('jsprf.controllers')
           },
           onError: function (err) {
             $scope.$apply(function () {
-              script.errorMessage = err.message.message
+              script.error = err.message.message
             })
           },
           onComplete: function (event) {
             $scope.$apply(function () {
+              var bench = event.target
               script.inProgress = false
-              script.results = String(event.target)
+              script.ops = Benchmark.formatNumber(bench.hz.toFixed(bench.hz < 100 ? 2 : 0)) + ' ops/sec (\xb1' + bench.stats.rme.toFixed(2) + '%)'
+              script.results = bench
             })
           }
         })
@@ -80,8 +100,7 @@ angular.module('jsprf.controllers')
           $scope.inProgress = true
         })
         .on('cycle', function (event) {
-          var winners = this.filter('fastest')
-          highlightWinners(winners)
+          highlightWinners(this)
         })
         .on('complete', function() {
           $scope.inProgress = false
@@ -89,8 +108,11 @@ angular.module('jsprf.controllers')
         // run async
         .run({ 'async': true })
     }
-    
+
     $scope.cancelPerfs = function () {
         benchmarkSuite.abort()
+    }
+    $scope.switchToEdit = function () {
+        $scope.editMode = true
     }
   }])
